122202116aba62edc5c1436ec6b663e78249dc43
Work <work@Crankss-MacBook-Pro.local>
2022-10-05 22:29:27 -0700

    second

---
+++ src/Components/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import logo from '../logo.png'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className='container'>
+  <a className="navbar-brand" href="#"><img className='logo'  src={logo} alt='logo...' ></img></a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav ms-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Shorts</a>
+      </li>
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Poems</a>
+      </li>
+      <li className="nav-item active">
+        <a className="nav-link" href="#">About</a>
+      </li>
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
